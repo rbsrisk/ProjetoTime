02c865bad6c88b43d883c161a273d140c0a57ef5
rbsrisk <rafael.bsantos@outlook.com>
2020-07-31 16:53:35 -0300

    Primeiro Commit

---
+++ src/Time.java
@@ -0,0 +1,50 @@
+
+public class Time {
+
+	private int Hora;
+	private int Minuto;
+	private int Segundo;
+	
+	
+	public Time(int Hora, int Minuto, int Segundo) {
+		
+		this.Hora = Hora;
+		this.Minuto = Minuto;
+		this.Segundo = Segundo;
+	}
+
+
+	public int getHora() {
+		return Hora;
+	}
+
+
+	public void setHora(int hora) {
+		Hora = hora;
+	}
+
+
+	public int getMinuto() {
+		return Minuto;
+	}
+
+
+	public void setMinuto(int minuto) {
+		Minuto = minuto;
+	}
+
+
+	public int getSegundo() {
+		return Segundo;
+	}
+
+
+	public void setSegundo(int segundo) {
+		Segundo = segundo;
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/TimeTeste.java
@@ -0,0 +1,17 @@
+
+public class TimeTeste {
+	
+	public static void main(String args[]) {			
+		
+		
+		Time t = new Time (10,30,25);
+		System.out.println(t.getHora());
+		System.out.println(t.getMinuto());
+		System.out.println(t.getSegundo());
+		
+		System.out.println("Hora Universal: "+t.getHora+":"+t.getMinuto+":"+t.getSegundo);
+		
+			
+		}
+
+}
